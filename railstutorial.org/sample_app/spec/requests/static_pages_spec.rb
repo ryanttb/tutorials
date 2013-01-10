06e805d0bb2cf07fefb8a0117d30dff11596fd36
Ryan Westphal <ryan@trippingthebits.com>
2013-01-10 16:53:49 -0500

    more programmer like syntax

--- railstutorial.org/sample_app/spec/requests/static_pages_spec.rb
+++ railstutorial.org/sample_app/spec/requests/static_pages_spec.rb
@@ -1,56 +1,49 @@
 require( "spec_helper" );
 
-describe "static_pages" do
+describe( "static_pages" ) {
   let (:base_title) { "Ruby on Rails Tutorial Sample App" };
 
-  describe "about" do
-    it "should have the content 'About'" do
-      visit( "/static_pages/about" );
+  describe( "about" ) {
+    it( "should have the content 'About'" ) {
+      visit( about_path );
       page.should( have_content( "About" ) );
-    end
-  end
+    }
+  }
 
-  describe "contact" do
-    it "should have the title ':base_title | Contact Us'" do
-      visit( "/static_pages/contact" );
-      page.should( have_selector( "title", {
-        text: "#{base_title} | Contact Us"
-      } ) );
-    end
+  describe( "contact" ) {
+    it( "should have the title ':base_title | Contact Us'" ) {
+      visit( contact_path );
+      page.should( have_selector( "title", { text: "#{base_title} | Contact Us" } ) );
+    }
 
-    it "should have 'Contact Us' in the header" do
-      visit( "/static_pages/contact" );
-      page.should( have_selector( "h1", {
-        text: "Contact Us"
-      } ) );
-    end
-  end
+    it( "should have 'Contact Us' in the header" ) {
+      visit( contact_path );
+      page.should( have_selector( "h1", { text: "Contact Us" } ) );
+    }
+  }
 
-  describe "help" do
-    it "should have the content 'Help'" do
-      visit( "/static_pages/help" );
+  describe( "help" ) {
+    it( "should have the content 'Help'" ) {
+      visit( help_path );
       page.should( have_content( "Help" ) );
-    end
-  end
+    }
+  }
 
-  describe "home" do
-    it "should have 'Home' in the title" do
-      visit( "/static_pages/home" );
-      page.should( have_selector( "title", {
-        text: "Home"
-      } ) );
-    end
+  describe( "home" ) {
+    before( :each ) {
+      visit( root_path )
+    }
 
-    it "should have a sign up link" do
-      visit( "/static_pages/home" );
-      page.should( have_selector( "a", {
-        text: "Sign up"
-      } ) );
-    end
+    it( "should have 'Home' in the title" ) {
+      page.should( have_selector( "title", { text: "Home" } ) );
+    }
 
-    it "should have navigation" do
-      visit( "/static_pages/home" );
+    it( "should have a sign up link" ) {
+      page.should( have_selector( "a", { text: "Sign up" } ) );
+    }
+
+    it( "should have navigation" ) {
       page.should( have_selector( "nav" ) );
-    end
-  end
-end
+    }
+  }
+}
