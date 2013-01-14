0f4bdd50484d0259d90d92c4ab6326ccd9c631ff
Ryan Westphal <ryan@trippingthebits.com>
2013-01-14 10:19:56 -0500

    email validation

--- railstutorial.org/sample_app/spec/models/user_spec.rb
+++ railstutorial.org/sample_app/spec/models/user_spec.rb
@@ -27,16 +27,52 @@ describe( "User model" ) {
 
   it { should( be_valid() ); }
 
-  describe( "blank name" ) {
+  describe( "when name is blank" ) {
     before { @user.name = " " }
 
     it { should_not( be_valid() ); }
   }
 
-  describe( "blank email" ) {
+  describe( "when name is too long" ) {
+    before { @user.name = "a" * 51 }
+
+    it { should_not( be_valid() ); }
+  }
+
+  describe( "when email is blank " ) {
     before { @user.email = " " }
 
     it { should_not( be_valid() ); }
+  }
+
+  describe( "when email format is invalid" ) {
+    it {
+      addresses = %w[ user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com ];
+      addresses.each { |invalid_address|
+        @user.email = invalid_address;
+        should_not( be_valid() );
+      }
+    }
+  }
+
+  describe( "when email format is valid" ) {
+    it {
+      addresses = %w[ user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn ];
+      addresses.each { |valid_address|
+        @user.email = valid_address;
+        should( be_valid( ) );
+      }
+    }
+  }
+
+  describe( "when email is already taken" ) {
+    before {
+      user_with_same_email = @user.dup( );
+      user_with_same_email.email = @user.email.upcase( );
+      user_with_same_email.save( );
+    }
+
+    it { should_not( be_valid( ) ) }
   }
 
   #pending( "add some examples to (or delete) #{__FILE__}" );
