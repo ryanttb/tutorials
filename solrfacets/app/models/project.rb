bd7b2c61b70ddca206aec43e166cedda5a277e0c
Ryan Westphal <ryan@trippingthebits.com>
2013-07-23 17:13:36 -0400

    solr: rake sunspot_rails:install

--- solrfacets/app/models/project.rb
+++ solrfacets/app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :repository_url, :title, :tag_list
   acts_as_taggable_on :tags
+
+  searchable :auto_index => true, :auto_remove => true do
+    string :title, :stored => true
+    string :description, :stored => true
+    string :tag_list, :multiple => true, :stored => true
+  end
 end
