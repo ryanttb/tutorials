bb1740f3d1df576b63ed96a11a430f41279632c3
Ryan Westphal <ryan@trippingthebits.com>
2013-07-23 17:01:43 -0400

    add tagging and test data

--- solrfacets/app/models/project.rb
+++ solrfacets/app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  attr_accessible :description, :repository_url, :title
+  attr_accessible :description, :repository_url, :title, :tag_list
   acts_as_taggable_on :tags
 end
